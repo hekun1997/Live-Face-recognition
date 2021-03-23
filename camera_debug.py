1010af4076d4790286bfc80d58e6afff1381dc2a
hekun1997 <48910648+hekun1997@users.noreply.github.com>
2021-03-23 12:44:56 +0800

    add get_default_video_id.

--- camera_debug.py
+++ camera_debug.py
@@ -1,8 +1,21 @@
 import cv2
 
-if __name__ == '__main__':
+def get_default_video():
+    ID = 0
+    while (1):
+        camera_capture = cv2.VideoCapture(ID)
+        ret, frame = camera_capture.read()
 
-    cameraCapture = cv2.VideoCapture(0)
+        if not ret:
+            ID += 1
+        else:
+            print(ID)
+            break
+    return ID
+
+if __name__ == '__main__':
+    Id = 0#get_default_video()
+    cameraCapture = cv2.VideoCapture(Id)
     success, frame = cameraCapture.read()
 
     while success and cv2.waitKey(1) == -1:
